FlareCoding/StelluxOS

xHCI - Check if command ring is full before enqueueing command

kernel/src/drivers/usb/xhci/xhci.cpp
@@ -472,7 +472,10 @@ xhci_command_completion_trb_t* xhci_driver::_send_command(xhci_trb_t* trb, uint3
     mutex_guard guard(s_xhc_command_lock);
 
     // Enqueue the TRB
-    m_command_ring->enqueue(trb);
+    if (!m_command_ring->enqueue(trb)) {
+        xhci_warn("Failed to enqueue command. Command ring is full.");
+        return nullptr;
+    };
 
     // Ring the command doorbell
     m_doorbell_manager->ring_command_doorbell();
@@ -504,6 +507,9 @@ xhci_command_completion_trb_t* xhci_driver::_send_command(xhci_trb_t* trb, uint3
         xhci_warn("Command TRB failed with error: %s\n", trb_completion_code_to_string(completion_trb->completion_code));
         return nullptr;
     }
+
+    // Update the command ring dequeue pointer
+    m_command_ring->process_event(completion_trb);
 
     return completion_trb;
 }

FlareCoding/stellux-xhci-tutorial

kernel/src/drivers/usb/xhci/xhci.cpp
@@ -438,7 +438,10 @@ void xhci_driver::_acknowledge_irq(uint8_t interrupter) {
 
 xhci_command_completion_trb_t* xhci_driver::_send_command_trb(xhci_trb_t* cmd_trb, uint32_t timeout_ms) {
     // Enqueue the TRB
-    m_command_ring->enqueue(cmd_trb);
+    if (!m_command_ring->enqueue(cmd_trb)) {
+        serial::printf("Failed to enqueue command. Command ring is full.");
+        return nullptr;
+    };
 
     // Ring the command doorbell
     m_doorbell_manager->ring_command_doorbell();
@@ -472,6 +475,9 @@ xhci_command_completion_trb_t* xhci_driver::_send_command_trb(xhci_trb_t* cmd_tr
         serial::printf("Command TRB failed with error: %s\n", trb_completion_code_to_string(completion_trb->completion_code));
         return nullptr;
     }
+
+    // Update the command ring dequeue pointer
+    m_command_ring->process_event(completion_trb);
 
     return completion_trb;
 }